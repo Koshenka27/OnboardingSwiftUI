2866e7e22c62924549ec8fd63e497f613ac19fdc
Logan Koshenka <koshenka27@gmail.com>
2021-07-27 07:16:28 -0400

    Initial Commit

---
+++ OnboardingTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnboardingTutorial
+//
+//  Created by Logan Koshenka on 7/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnboardingTutorial/OnboardingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingTutorialApp.swift
+//  OnboardingTutorial
+//
+//  Created by Logan Koshenka on 7/27/21.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
